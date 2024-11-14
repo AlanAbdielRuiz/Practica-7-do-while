0ebcc3fceb532badf76804c43d3e6a18734b50fd
Alan Abdiel <Alan Abdiel@LAPTOP-DA4AJN7D>
2024-11-14 16:12:00 -0600

    Agregar archivos de proyecto.

---
+++ Practica 7 do while/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Practica_7_do_while
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            BotonImprime = new Button();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Emoji", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(117, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(613, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Imprime números de un rango específico y calcula la suma";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Emoji", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(134, 78);
+            label2.Name = "label2";
+            label2.Size = new Size(110, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Rango inferior";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Emoji", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(434, 84);
+            label3.Name = "label3";
+            label3.Size = new Size(119, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Rango Superior";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Emoji", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(503, 385);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 21);
+            label4.TabIndex = 3;
+            label4.Text = "Suma";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI Emoji", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(262, 75);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 29);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI Emoji", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(575, 81);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 29);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI Emoji", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(575, 385);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 29);
+            textBox3.TabIndex = 6;
+            // 
+            // BotonImprime
+            // 
+            BotonImprime.Font = new Font("Segoe UI Emoji", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            BotonImprime.Location = new Point(262, 206);
+            BotonImprime.Name = "BotonImprime";
+            BotonImprime.Size = new Size(100, 39);
+            BotonImprime.TabIndex = 7;
+            BotonImprime.Text = "Imprime";
+            BotonImprime.UseVisualStyleBackColor = true;
+            BotonImprime.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Segoe UI Emoji", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 17;
+            listBox1.Location = new Point(566, 123);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(127, 242);
+            listBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(BotonImprime);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Suma de un rango";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button BotonImprime;
+        private ListBox listBox1;
+    }
+}
